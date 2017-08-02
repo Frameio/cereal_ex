d6ad7c11300b22c480c3ddb52f5548935f783143
Chris Bell <cjbell1988@gmail.com>
2017-08-02 15:20:45 -0400

    Fix issue with errors module

--- lib/cereal/builders/errors.ex
+++ lib/cereal/builders/errors.ex
@@ -4,9 +4,9 @@ defmodule Cereal.Builders.Errors do
 
   alias Cereal.Builders.Error
 
-  @spec build(Cereal.Context.t) :: Errors.t
+  @spec build(Cereal.Context.t) :: __MODULE__.t
   def build(%{serializer: serializer} = context) do
-    struct(Errors, %{})
+    struct(__MODULE__, %{})
     |> Map.put(:title, serializer.title(context.conn))
     |> Map.put(:errors, Error.build(context))
     |> Map.put(:status, serializer.status(context.conn))
