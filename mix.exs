70b5a845773b241b75d7d1252a44b179cd79c88d
Zach Smith <zd@zdsmith.com>
2020-01-24 13:03:14 -0500

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Cereal.Mixfile do
   defp deps do
     [
       {:ecto, ">= 2.0.0"},
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:scrivener, "~> 1.2 or ~> 2.0", optional: true},
       {:plug_cowboy, "~> 2.0.2"}
     ]
